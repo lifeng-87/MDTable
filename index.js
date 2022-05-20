cfea077f01b75966084492fb0a27a0b6bae8a2ab
lifeng87 <oscarcoll.930714@gmail.com>
2022-05-20 17:57:47 +0800

    finish

--- index.js
+++ index.js
@@ -1,66 +1,157 @@
-require("dotenv").config()
+require("dotenv").config();
 const axios = require("axios");
 const cheerio = require("cheerio");
-const iconv = require("iconv-lite")
+const iconv = require("iconv-lite");
 
+//env: STDID, STDPWD
 const url = "http://libauto.mingdao.edu.tw/AACourses/Web/wLogin.php";
-const username = process.env.USERNAME;
-const passwd = process.env.PASSWD;
+const delay = 1000; //爬取Meet網址的延遲(不要設太低，學校server會炸)
+
 start(url);
 
+/**
+ * {name, teacher, online:{url, code}}
+ */
 async function start(url) {
-	const html = await login(url);
-	const table = getTable(html);
-	console.log(table)
+	try {
+		console.log("Getting Table...");
+		const data = await getData(url);
+		console.log("Finish!");
+		console.log("Table:", data);
+	} catch (err) {
+		console.error(err);
+		return;
+	}
 }
 
-function getTable(html){
-	const table = [];
-	const $ = cheerio.load(html);
-	//F_行_列(start=1)
-	for (var x = 1; x <= 5; x++) {
-		const row = [];
-		for (var y = 1; y <= 8; y++) {
-			row[y-1] = $(`#F_${x}_${y} table tbody tr td span div div.subj`).text()
+async function getData(url) {
+	try {
+		async function getCookie() {
+			try {
+				console.log("Getting cookie...");
+				const res = await axios({
+					responseType: "arraybuffer",
+					method: "get",
+					url: url,
+					headers: {
+						"User-Agent":
+							"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47",
+					},
+					transformResponse: [
+						(data) => {
+							return iconv.decode(Buffer.from(data), "big5");
+						},
+					],
+				});
+				const returnCookie = await res.headers["set-cookie"][0];
+				const cookie = returnCookie.slice(0, 36);
+				console.log("Getting cookie finish!");
+				return cookie;
+			} catch (err) {
+				console.log(err);
+				return;
+			}
 		}
-		table[x-1] = row;
+		const cookie = await getCookie();
+
+		async function getHtml() {
+			try {
+				console.log("Getting html...");
+				const res = await axios({
+					responseType: "arraybuffer",
+					method: "post",
+					url: url,
+					headers: {
+						"Cookie": cookie,
+						"User-Agent":
+							"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47",
+					},
+					data: `sureReg=YES&goURL=&accessWay=ACCOUNT&HTTP_REFERER=http%3A%2F%2Flibauto.mingdao.edu.tw%2FAACourses%2FWeb%2FqWTT.php&wRole=STD&stdID=${encodeURI(
+						process.env.STDID
+					)}&stdPWD=${encodeURI(process.env.STDPWD)}&uRFID=&Submit=%BDT%A9w%B5n%A4J`,
+					transformResponse: [
+						(data) => {
+							return iconv.decode(Buffer.from(data), "big5");
+						},
+					],
+				});
+				console.log("Getting html finish!");
+				return res.data;
+			} catch (err) {
+				console.log(err);
+				return;
+			}
+		}
+		const html = await getHtml();
+		const $ = cheerio.load(html);
+
+		async function getOnline(x, y, f_sPeriodsem) {
+			try {
+				console.log(`Getting online info... (day: ${x}), period: ${y})`);
+				const id = $(`#F_${x}_${y}_P div img`).attr("id").slice(6);
+				const res = await axios({
+					method: "get",
+					url: `http://libauto.mingdao.edu.tw/AACourses/Web/qVT.php?F_sPeriodsem=${f_sPeriodsem}&eID=${id}`,
+					headers: {
+						"Cookie": cookie,
+						"User-Agent":
+							"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47",
+					},
+					transformResponse: [
+						(data) => {
+							return iconv.decode(Buffer.from(data), "big5");
+						},
+					],
+				});
+
+				const form = cheerio.load(res.data);
+				const url = form(`#main div.vtC a`).text().trim();
+				const code = form(`#main div.vtC`).text().trim().split(/ +/)[1];
+				console.log(`Getting online info finish`);
+				return { url: url, code: code };
+			} catch (err) {
+				console.log(err);
+				return;
+			}
+		}
+
+		async function getTable() {
+			try {
+				const table = [];
+				const location = "table tbody tr td span div div.";
+				//F_行_列(start=1)
+				for (var x = 1; x <= 5; x++) {
+					const row = [];
+					for (var y = 1; y <= 8; y++) {
+						//共40次httprequest
+						const online = await getOnline(
+							x,
+							y,
+							$(`#F_sPeriodsem option:selected`).val()
+						);
+						row[y - 1] = {
+							name: $(`#F_${x}_${y} ${location}subj`).text(),
+							teacher: $(`#F_${x}_${y} ${location}tea`).text(),
+							online: online,
+						};
+						timeout(delay);
+					}
+					table[x - 1] = row;
+				}
+				return table;
+			} catch (err) {
+				console.log(err);
+				return;
+			}
+		}
+		const data = await getTable();
+		return data;
+	} catch (err) {
+		return console.log(err);
 	}
-	return table;
 }
 
-async function login(url) {
-	const getCookie = async (url) => {
-		const res = await axios({
-			responseType: 'arraybuffer',
-			method: "get",
-			url:url,
-			headers: {
-				"User-Agent":
-					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47",
-			},
-		});
-		const returnCookie = await res.headers["set-cookie"][0];
-		const cookie = returnCookie.slice(0, 36);
-		return cookie;
-	}
-	const cookie = await getCookie(url);
-	const res = await axios({
-		responseType: 'arraybuffer',
-		method: "post",
-		url:url,
-		headers: {
-			"accept-language": "zh-TW,zh;q=0.9",
-			"Content-Length": "193",
-			"Content-Type": "application/x-www-form-urlencoded",
-			"Cookie": cookie,
-			"User-Agent":
-				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47",
-		},
-		data: `sureReg=YES&goURL=&accessWay=ACCOUNT&HTTP_REFERER=http%3A%2F%2Flibauto.mingdao.edu.tw%2FAACourses%2FWeb%2FqWTT.php&wRole=STD&stdID=${username}&stdPWD=${passwd}&uRFID=&Submit=%BDT%A9w%B5n%A4J`,
-		transformResponse:[(data)=>{
-			return iconv.decode(Buffer.from(data), 'big5')
-		}]
-	});
-	const body = await res.data
-	return body;
+//delay
+function timeout(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
